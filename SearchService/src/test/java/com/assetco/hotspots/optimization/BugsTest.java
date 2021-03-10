1e7ca2508884171dd4dd40723d667a229c6eaf89
Marco Seguri <seguri@users.noreply.github.com>
2021-03-10 19:18:48 +0100

    Update BugsTest implementation

--- SearchService/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -2,24 +2,40 @@ package com.assetco.hotspots.optimization;
 
 import static com.assetco.hotspots.optimization.fixture.AssetFixture.asset;
 import static com.assetco.hotspots.optimization.fixture.AssetVendorFixture.assetVendor;
+import static com.assetco.search.results.AssetVendorRelationshipLevel.Partner;
 import static com.assetco.search.results.HotspotKey.Showcase;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.assetco.search.results.Asset;
+import com.assetco.search.results.AssetVendor;
 import com.assetco.search.results.HotspotKey;
+import com.assetco.search.results.SearchResults;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BugsTest {
 
+  private SearchResults searchResults;
+  private SearchResultHotspotOptimizer optimizer;
+
+  @BeforeEach
+  void setup() {
+    searchResults = new SearchResults();
+    optimizer = new SearchResultHotspotOptimizer();
+  }
+
   @Test
   void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
-    var partnerVendor = assetVendor();
-    var otherPartnerVendor = assetVendor();
-    var missing = asset(partnerVendor);
-    var otherMissing = asset(otherPartnerVendor);
-    var expected =
-        List.of(
-            asset(partnerVendor), asset(partnerVendor), asset(partnerVendor), asset(partnerVendor));
+    var partnerVendor = assetVendor(Partner);
+    var otherPartnerVendor = assetVendor(Partner);
+    var missing = givenAssetInResultsWithVendor(partnerVendor);
+    givenAssetInResultsWithVendor(otherPartnerVendor);
+    var expected = givenAssetsInResultsWithVendor(partnerVendor, 4);
 
     whenOptimize();
 
@@ -27,9 +43,30 @@ class BugsTest {
     thenHotspotHasExactly(Showcase, expected);
   }
 
-  private void whenOptimize() {}
+  private Asset givenAssetInResultsWithVendor(AssetVendor assetVendor) {
+    var asset = asset(assetVendor);
+    searchResults.addFound(asset);
+    return asset;
+  }
 
-  private void thenHotspotDoesNotHave(HotspotKey key, Asset... assets) {}
+  private List<Asset> givenAssetsInResultsWithVendor(AssetVendor assetVendor, int count) {
+    return IntStream.range(0, count)
+        .mapToObj(i -> givenAssetInResultsWithVendor(assetVendor))
+        .collect(Collectors.toList());
+  }
 
-  private void thenHotspotHasExactly(HotspotKey key, List<Asset> assets) {}
+  private void whenOptimize() {
+    optimizer.optimize(searchResults);
+  }
+
+  private void thenHotspotDoesNotHave(HotspotKey key, Asset... forbiddenAssets) {
+    var members = searchResults.getHotspot(key).getMembers();
+    Arrays.stream(forbiddenAssets)
+        .forEach(forbiddenAsset -> assertFalse(members.contains(forbiddenAsset)));
+  }
+
+  private void thenHotspotHasExactly(HotspotKey key, List<Asset> expected) {
+    var members = searchResults.getHotspot(key).getMembers().toArray(new Asset[0]);
+    assertArrayEquals(expected.toArray(new Asset[0]), members);
+  }
 }
--- SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
@@ -1,6 +1,7 @@
 package com.assetco.hotspots.optimization.fixture;
 
 import static com.assetco.hotspots.optimization.fixture.AssetPurchaseInfoFixture.assetPurchaseInfo;
+import static java.util.Collections.emptyList;
 
 import com.assetco.search.results.Asset;
 import com.assetco.search.results.AssetVendor;
@@ -10,6 +11,14 @@ public class AssetFixture {
   public static Asset asset(AssetVendor assetVendor) {
     var purchaseInfoLast30Days = assetPurchaseInfo();
     var purchaseInfoLast24Hours = assetPurchaseInfo();
-    return new Asset(null, null, null, null, purchaseInfoLast30Days, purchaseInfoLast24Hours, null, assetVendor);
+    return new Asset(
+        "ID",
+        "TITLE",
+        null,
+        null,
+        purchaseInfoLast30Days,
+        purchaseInfoLast24Hours,
+        emptyList(),
+        assetVendor);
   }
 }
