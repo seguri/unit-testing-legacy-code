953a94be4ee794833b8c67aa9d5feca9c6797b7b
Marco Seguri <seguri@users.noreply.github.com>
2021-03-14 21:33:10 +0100

    Update test fixtures

--- SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
@@ -1,7 +1,6 @@
 package com.assetco.hotspots.optimization.fixture;
 
 import static com.assetco.hotspots.optimization.fixture.AssetPurchaseInfoFixture.assetPurchaseInfo;
-import static java.util.Collections.emptyList;
 
 import com.assetco.search.results.Asset;
 import com.assetco.search.results.AssetTopic;
@@ -9,10 +8,6 @@ import com.assetco.search.results.AssetVendor;
 import java.util.List;
 
 public class AssetFixture {
-
-  public static Asset asset(AssetVendor vendor) {
-    return asset(vendor, emptyList());
-  }
 
   public static Asset asset(AssetVendor vendor, List<AssetTopic> topics) {
     var purchaseInfoLast30Days = assetPurchaseInfo();
