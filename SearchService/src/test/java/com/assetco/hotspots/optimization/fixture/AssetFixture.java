ebb8f148bcef6752d5ff78e42c80bb52fdf8d6a4
Marco Seguri <seguri@users.noreply.github.com>
2021-03-14 21:58:59 +0100

    Update test fixtures

--- SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetFixture.java
@@ -1,17 +1,36 @@
 package com.assetco.hotspots.optimization.fixture;
 
 import static com.assetco.hotspots.optimization.fixture.AssetPurchaseInfoFixture.assetPurchaseInfo;
+import static com.assetco.hotspots.optimization.fixture.AssetTopicFixture.emptyAssetTopics;
 
 import com.assetco.search.results.Asset;
+import com.assetco.search.results.AssetPurchaseInfo;
 import com.assetco.search.results.AssetTopic;
 import com.assetco.search.results.AssetVendor;
 import java.util.List;
 
 public class AssetFixture {
 
+  public static Asset asset(AssetVendor vendor) {
+    return asset(vendor, assetPurchaseInfo(), assetPurchaseInfo(), emptyAssetTopics());
+  }
+
   public static Asset asset(AssetVendor vendor, List<AssetTopic> topics) {
-    var purchaseInfoLast30Days = assetPurchaseInfo();
-    var purchaseInfoLast24Hours = assetPurchaseInfo();
+    return asset(vendor, assetPurchaseInfo(), assetPurchaseInfo(), topics);
+  }
+
+  public static Asset asset(
+      AssetVendor vendor,
+      AssetPurchaseInfo purchaseInfoLast30Days,
+      AssetPurchaseInfo purchaseInfoLast24Hours) {
+    return asset(vendor, purchaseInfoLast30Days, purchaseInfoLast24Hours, emptyAssetTopics());
+  }
+
+  public static Asset asset(
+      AssetVendor vendor,
+      AssetPurchaseInfo purchaseInfoLast30Days,
+      AssetPurchaseInfo purchaseInfoLast24Hours,
+      List<AssetTopic> topics) {
     return new Asset(
         "ID", "TITLE", null, null, purchaseInfoLast30Days, purchaseInfoLast24Hours, topics, vendor);
   }
--- SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetPurchaseInfoFixture.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/fixture/AssetPurchaseInfoFixture.java
@@ -9,11 +9,13 @@ public class AssetPurchaseInfoFixture {
 
   private static final Random RANDOM = new Random();
 
-  public static AssetPurchaseInfo assetPurchaseInfo() {
-    var timesShown = RANDOM.nextLong();
-    var timesPurchased = RANDOM.nextLong();
+  public static AssetPurchaseInfo assetPurchaseInfo(long timesShown, long timesPurchased) {
     var totalRevenue = money();
     var totalRoyaltiesOwed = money();
     return new AssetPurchaseInfo(timesShown, timesPurchased, totalRevenue, totalRoyaltiesOwed);
+  }
+
+  public static AssetPurchaseInfo assetPurchaseInfo() {
+    return assetPurchaseInfo(RANDOM.nextLong(), RANDOM.nextLong());
   }
 }
