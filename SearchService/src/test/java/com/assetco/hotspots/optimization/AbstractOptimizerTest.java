0b115b9cd3bb857bd2c270b70f614b1287575c60
Marco Seguri <seguri@users.noreply.github.com>
2021-03-19 00:07:39 +0100

    Refactor existing tests

--- SearchService/src/test/java/com/assetco/hotspots/optimization/AbstractOptimizerTest.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/AbstractOptimizerTest.java
@@ -1,23 +1,43 @@
 package com.assetco.hotspots.optimization;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static com.assetco.hotspots.optimization.fixture.AssetVendorFixture.assetVendor;
+import static com.assetco.search.results.AssetVendorRelationshipLevel.Basic;
+import static com.assetco.search.results.AssetVendorRelationshipLevel.Gold;
+import static com.assetco.search.results.AssetVendorRelationshipLevel.Partner;
+import static com.assetco.search.results.AssetVendorRelationshipLevel.Silver;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.assetco.search.results.Asset;
+import com.assetco.search.results.AssetVendor;
 import com.assetco.search.results.HotspotKey;
 import com.assetco.search.results.SearchResults;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 
 abstract class AbstractOptimizerTest {
+  protected static final AssetVendor PARTNER_VENDOR = assetVendor(Partner);
+  protected static final AssetVendor GOLD_VENDOR = assetVendor(Gold);
+  protected static final AssetVendor SILVER_VENDOR = assetVendor(Silver);
+  protected static final AssetVendor BASIC_VENDOR = assetVendor(Basic);
 
+  protected Map<Asset, Boolean> dealEligibility;
   protected SearchResults searchResults;
   protected SearchResultHotspotOptimizer sut;
 
   @BeforeEach
   void setup() {
+    dealEligibility = new HashMap<>();
     searchResults = new SearchResults();
     sut = new SearchResultHotspotOptimizer();
+  }
+
+  protected void thenHotspotHas(HotspotKey key, Asset... expected) {
+    thenHotspotHas(key, Arrays.asList(expected));
   }
 
   protected void thenHotspotHas(HotspotKey key, List<Asset> expected) {
@@ -27,12 +47,23 @@ abstract class AbstractOptimizerTest {
     }
   }
 
-  protected void thenHotspotHasExactly(HotspotKey key, Asset... expected) {
+  protected void thenHotspotHasNot(HotspotKey key, Asset... forbidden) {
+    thenHotspotHasNot(key, Arrays.asList(forbidden));
+  }
+
+  protected void thenHotspotHasNot(HotspotKey key, List<Asset> forbidden) {
     var members = searchResults.getHotspot(key).getMembers();
-    assertArrayEquals(expected, members.toArray(new Asset[0]));
+    for (var asset : forbidden) {
+      assertFalse(members.contains(asset));
+    }
+  }
+
+  protected void thenHotspotHasExactly(HotspotKey key, Asset... expected) {
+    thenHotspotHasExactly(key, Arrays.asList(expected));
   }
 
   protected void thenHotspotHasExactly(HotspotKey key, List<Asset> expected) {
-    thenHotspotHasExactly(key, expected.toArray(new Asset[0]));
+    var members = searchResults.getHotspot(key).getMembers();
+    assertIterableEquals(expected, members);
   }
 }
--- SearchService/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
+++ SearchService/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -3,7 +3,6 @@ package com.assetco.hotspots.optimization;
 import static com.assetco.hotspots.optimization.fixture.AssetFixture.asset;
 import static com.assetco.hotspots.optimization.fixture.AssetPurchaseInfoFixture.assetPurchaseInfo;
 import static com.assetco.hotspots.optimization.fixture.AssetTopicFixture.assetTopic;
-import static com.assetco.hotspots.optimization.fixture.AssetTopicFixture.emptyAssetTopics;
 import static com.assetco.hotspots.optimization.fixture.AssetVendorFixture.assetVendor;
 import static com.assetco.search.results.AssetVendorRelationshipLevel.Basic;
 import static com.assetco.search.results.AssetVendorRelationshipLevel.Partner;
@@ -12,22 +11,22 @@ import static com.assetco.search.results.HotspotKey.Highlight;
 import static com.assetco.search.results.HotspotKey.Showcase;
 
 import com.assetco.search.results.Asset;
-import com.assetco.search.results.AssetTopic;
-import com.assetco.search.results.AssetVendor;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 class BugsTest extends AbstractOptimizerTest {
 
   @Test
   void prevailingPartnerReceivesFirstFiveItemsInShowcase() {
-    var partnerVendor = assetVendor(Partner);
     var otherPartnerVendor = assetVendor(Partner);
-    var expected = assetsInSearchResults(partnerVendor, 4);
-    expected.add(assetInSearchResults(partnerVendor));
-    assetInSearchResults(otherPartnerVendor);
+    var expected = new ArrayList<Asset>();
+    expected.add(assetInSearchResults(asset(PARTNER_VENDOR)));
+    assetInSearchResults(asset(otherPartnerVendor));
+    expected.add(assetInSearchResults(asset(PARTNER_VENDOR)));
+    expected.add(assetInSearchResults(asset(PARTNER_VENDOR)));
+    expected.add(assetInSearchResults(asset(PARTNER_VENDOR)));
+    expected.add(assetInSearchResults(asset(PARTNER_VENDOR)));
 
     sut.optimize(searchResults);
 
@@ -36,13 +35,16 @@ class BugsTest extends AbstractOptimizerTest {
 
   @Test
   void allItemsDeservingHighlightAreHighlighted() {
-    var basicVendor = assetVendor(Basic);
     var hotTopic = assetTopic();
     var topic = assetTopic();
     sut.setHotTopics(() -> List.of(hotTopic, topic));
-    var expected = assetsInSearchResults(basicVendor, topic, 2);
-    assetsInSearchResults(basicVendor, hotTopic, 3);
-    expected.add(assetInSearchResults(basicVendor, topic));
+    var expected = new ArrayList<Asset>();
+    expected.add(assetInSearchResults(asset(BASIC_VENDOR, topic)));
+    expected.add(assetInSearchResults(asset(BASIC_VENDOR, topic)));
+    assetInSearchResults(asset(BASIC_VENDOR, hotTopic));
+    assetInSearchResults(asset(BASIC_VENDOR, hotTopic));
+    assetInSearchResults(asset(BASIC_VENDOR, hotTopic));
+    expected.add(assetInSearchResults(asset(BASIC_VENDOR, topic)));
 
     sut.optimize(searchResults);
 
@@ -54,40 +56,16 @@ class BugsTest extends AbstractOptimizerTest {
     var basicVendor = assetVendor(Basic);
     var purchaseInfoLast30Days = assetPurchaseInfo(50000, 50000);
     var purchaseInfoLast24Hours = assetPurchaseInfo(4000, 4000);
-    var asset = asset(basicVendor, purchaseInfoLast30Days, purchaseInfoLast24Hours);
-    searchResults.addFound(asset);
+    var asset =
+        assetInSearchResults(asset(basicVendor, purchaseInfoLast30Days, purchaseInfoLast24Hours));
 
     sut.optimize(searchResults);
 
     thenHotspotHasExactly(HighValue, asset);
   }
 
-  private Asset assetInSearchResults(AssetVendor vendor) {
-    return assetInSearchResults(vendor, emptyAssetTopics());
-  }
-
-  private Asset assetInSearchResults(AssetVendor vendor, AssetTopic topic) {
-    return assetInSearchResults(vendor, List.of(topic));
-  }
-
-  private Asset assetInSearchResults(AssetVendor vendor, List<AssetTopic> topics) {
-    var asset = asset(vendor, topics);
+  private Asset assetInSearchResults(Asset asset) {
     searchResults.addFound(asset);
     return asset;
-  }
-
-  private List<Asset> assetsInSearchResults(AssetVendor vendor, int count) {
-    return assetsInSearchResults(vendor, emptyAssetTopics(), count);
-  }
-
-  private List<Asset> assetsInSearchResults(AssetVendor vendor, AssetTopic topic, int count) {
-    return assetsInSearchResults(vendor, List.of(topic), count);
-  }
-
-  private List<Asset> assetsInSearchResults(
-      AssetVendor vendor, List<AssetTopic> topics, int count) {
-    return IntStream.range(0, count)
-        .mapToObj(i -> assetInSearchResults(vendor, topics))
-        .collect(Collectors.toList());
   }
 }
