4c486d73dea0014141eff3e1e7e8b4a5ba714ff8
Marco Seguri <seguri@users.noreply.github.com>
2021-03-20 19:43:44 +0100

    Update test fixtures

--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/fixture/AssetVendorFixture.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/fixture/AssetVendorFixture.java
@@ -1,16 +1,10 @@
 package com.assetco.hotspots.optimization.fixture;
 
-import static com.assetco.search.results.AssetVendorRelationshipLevel.Basic;
-
 import com.assetco.search.results.AssetVendor;
 import com.assetco.search.results.AssetVendorRelationshipLevel;
 import java.util.UUID;
 
 public class AssetVendorFixture {
-
-  public static AssetVendor assetVendor() {
-    return assetVendor(Basic);
-  }
 
   public static AssetVendor assetVendor(AssetVendorRelationshipLevel level) {
     return new AssetVendor(UUID.randomUUID().toString(), "DISPLAY_NAME", level, 0);
